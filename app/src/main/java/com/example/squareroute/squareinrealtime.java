1d94d4732896ecb9490e9768f6525716129147d3
darlenesushi <darlene.s15402@gmail.com>
2021-03-01 18:38:44 +0100

    Depot realtime

--- app/src/main/java/com/example/squareroute/squareinrealtime.java
+++ app/src/main/java/com/example/squareroute/squareinrealtime.java
@@ -1,15 +1,37 @@
 package com.example.squareroute;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class squareinrealtime extends AppCompatActivity {
+
+    ImageButton metro,rer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTheme(R.style.Theme_SquareRoute);
         setContentView(R.layout.activity_squareinrealtime);
+
+        metro = findViewById(R.id.btn_metro);
+        rer = findViewById(R.id.btn_rer);
+
+        metro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              //  startActivity(new Intent(getApplicationContext(),squareinrealtime.class));
+            }
+        });
+        rer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //  startActivity(new Intent(getApplicationContext(),squareinrealtime.class));
+            }
+        });
     }
+
 }
