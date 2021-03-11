704c5fa0625458267dff5a47669069b2ee30ad0a
christian-gamo <72939585+christian-gamo@users.noreply.github.com>
2021-03-11 01:35:22 +0100

    google maps 2 aled

--- app/src/main/java/com/example/squareroute/Dashboard.java
+++ app/src/main/java/com/example/squareroute/Dashboard.java
@@ -1,20 +1,33 @@
 package com.example.squareroute;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 
 public class Dashboard extends AppCompatActivity {
-    Button logout, checkEmail;
+    Button logout, checkEmail,localisation;
     ImageButton activity6,activity2,activity3;
     FirebaseAuth mauth;
 
@@ -32,31 +45,38 @@ public class Dashboard extends AppCompatActivity {
         activity6 = findViewById(R.id.btn_squarerealtime);
         activity2 = findViewById(R.id.btn_school);
         activity3 = findViewById(R.id.btn_library);
+        localisation = findViewById(R.id.btn_localisation);
 
-        if(!mauth.getCurrentUser().isEmailVerified()){
+
+        if (!mauth.getCurrentUser().isEmailVerified()) {
             checkEmail.setVisibility(View.VISIBLE);
         }
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              mauth.signOut();
-              signOutUser();
+                mauth.signOut();
+                signOutUser();
             }
         });
 
         checkEmail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mauth.getCurrentUser().sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>(){
+                mauth.getCurrentUser().sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
-                    public void onSuccess(Void aVoid){
-                        Toast.makeText(Dashboard.this,"Email de vérification envoyé", Toast.LENGTH_SHORT).show();
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(Dashboard.this, "Email de vérification envoyé", Toast.LENGTH_SHORT).show();
                         checkEmail.setVisibility(View.GONE);
                     }
                 });
             }
         });
+
+        if(ContextCompat.checkSelfPermission(Dashboard.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            localisation.setVisibility(View.GONE);
+        }
+
         activity6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -66,24 +86,73 @@ public class Dashboard extends AppCompatActivity {
         activity2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), MapsActivityUniversity.class));
+                if(ContextCompat.checkSelfPermission(Dashboard.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+                    startActivity(new Intent(getApplicationContext(), MapsActivityUniversity.class));
+                else
+                    Toast.makeText(Dashboard.this, "Veuillez autoriser la localisation", Toast.LENGTH_SHORT).show();
             }
         });
         activity3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), MapsActivityBibliotheque.class));
+                if(ContextCompat.checkSelfPermission(Dashboard.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+                    startActivity(new Intent(getApplicationContext(), MapsActivityBibliotheque.class));
+                else
+                    Toast.makeText(Dashboard.this, "Veuillez autoriser la localisation", Toast.LENGTH_SHORT).show();
             }
         });
+
+        localisation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dexter.withActivity(Dashboard.this)
+                        .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                        .withListener(new PermissionListener() {
+                            @Override
+                            public void onPermissionGranted(PermissionGrantedResponse response) {
+                                localisation.setVisibility(View.GONE);
+                                Toast.makeText(Dashboard.this, "Permission granted", Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onPermissionDenied(PermissionDeniedResponse response) {
+                                if (response.isPermanentlyDenied()) {
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(Dashboard.this);
+                                    builder.setTitle("Permission Denied")
+                                            .setMessage("Permission to access device location is permanently denied. you need to go to setting to allow the permission.")
+                                            .setNegativeButton("Cancel", null)
+                                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                                @Override
+                                                public void onClick(DialogInterface dialog, int which) {
+                                                    Intent intent = new Intent();
+                                                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                                    intent.setData(Uri.fromParts("package", getPackageName(), null));
+                                                }
+                                            })
+                                            .show();
+                                } else {
+                                    Toast.makeText(Dashboard.this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+
+                            @Override
+                            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                                token.continuePermissionRequest();
+                            }
+                        })
+                        .check();
+            }
+        });
+
+
     }
-    private void signOutUser() {
-        Intent mainActivity = new Intent(Dashboard.this,MainActivity.class);
+
+    private void signOutUser (){
+        Intent mainActivity = new Intent(Dashboard.this, MainActivity.class);
         mainActivity.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         Toast.makeText(Dashboard.this, "Déconnexion réussie !", Toast.LENGTH_SHORT).show();
         startActivity(mainActivity);
         Toast.makeText(Dashboard.this, "Déconnexion réussie !", Toast.LENGTH_SHORT).show();
         finish();
-
-
     }
 }
